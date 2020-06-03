a881e4f844095ce33e94b7b7dccb985186688856
SingDogYww <2281435866@qq.com>
2020-06-03 19:56:14 +0800

    测试新建分支

--- learn-DP-tanke/src/main/java/com/cxyxh/c03_tank/Main.java
+++ learn-DP-tanke/src/main/java/com/cxyxh/c03_tank/Main.java
@@ -3,6 +3,7 @@ package com.cxyxh.c03_tank;
 import com.cxyxh.c03_tank.TankFrame;
 
 public class Main {
+    //测试新建分支
     public static void main(String[] args) throws InterruptedException {
         TankFrame frame = new TankFrame();
         while (true){
